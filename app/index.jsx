b4327ab4d54d23bd1f15bda6b2f38cf11c5949d9
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-24 20:58:58 +0530

    create welcome screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,44 @@
-import { Text, View } from 'react-native'
+import { Image, ScrollView, StatusBar, Text, View } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router'
+import { images } from "../constants";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Welcome = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl font-pblack'>Welcome</Text>
-      <Link
-        href='/home'
-        style={{ color: 'blue' }}
-      >
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{
+        height: "100%",
+      }}>
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   )
 }
 
