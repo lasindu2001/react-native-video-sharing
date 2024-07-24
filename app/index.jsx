d0eac1ad8a6cb42d89194922fb022cac36d9e073
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-24 18:24:06 +0530

    config nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 
 const Welcome = () => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl'>Welcome</Text>
       <Link
         href='/profile'
         style={{ color: 'blue' }}
@@ -17,12 +17,3 @@ const Welcome = () => {
 }
 
 export default Welcome
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
