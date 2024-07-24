dbef7b00017f3369c4efc4244ee239726580e1c0
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-24 17:33:50 +0530

    slot navigation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Slot } from 'expo-router'
+import { Text } from 'react-native'
 
 const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>RootLayout</Text>
-    </View>
+    <>
+      <Text>Header</Text>
+      <Slot />
+      <Text>Footer</Text>
+    </>
   )
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
