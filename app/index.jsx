6f9da0df1944649b72e2c4bc3be41bd944b59716
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-24 18:35:42 +0530

    add assets, constants and config fonts

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router'
 const Welcome = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>Welcome</Text>
+      <Text className='text-3xl font-pblack'>Welcome</Text>
       <Link
         href='/profile'
         style={{ color: 'blue' }}
