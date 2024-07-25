964269b372ff766788859373e2084142d9d00921
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-26 01:10:24 +0530

    complete trending component and implement video playing functionality

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { icons } from '../constants'
+import { ResizeMode, Video } from 'expo-av';
 
 const VideoCard = ({ title, creator, avatar, thumbnail, video }) => {
     const [play, setPlay] = useState(false);
@@ -40,7 +41,18 @@ const VideoCard = ({ title, creator, avatar, thumbnail, video }) => {
                 </View>
             </View>
             {play ? (
-                <Text>Playing</Text>
+                <Video
+                    source={{ uri: video }}
+                    className="w-full h-60 rounded-xl mt-3"
+                    resizeMode={ResizeMode.CONTAIN}
+                    useNativeControls
+                    shouldPlay
+                    onPlaybackStatusUpdate={(status) => {
+                        if (status.didJustFinish) {
+                            setPlay(false);
+                        }
+                    }}
+                />
             ) : (
                 <TouchableOpacity
                     activeOpacity={0.7}
