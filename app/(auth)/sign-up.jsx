619874991cba524beb0b9acf0f02fa057e6cd5d5
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-25 12:57:18 +0530

    create signup functionality

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,22 +1,33 @@
-import { View, Text, ScrollView, Dimensions, Image } from 'react-native'
+import { View, Text, ScrollView, Dimensions, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants";
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { createUser } from '../../lib/appwrite';
 
 const SignUp = () => {
-  const [isSubmitting, setSubmitting] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [form, setForm] = useState({
     username: "",
     email: "",
     password: "",
   });
 
-  const submit = () => {
-    createUser()
+  const submit = async () => {
+    if (form.username === "" || form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+    setIsSubmitting(true)
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   return (
