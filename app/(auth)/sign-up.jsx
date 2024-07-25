79298f89973f4424b664e43b2de0385a3a15fc6d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-25 10:55:14 +0530

    config appwrite and create first user

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -5,6 +5,7 @@ import { images } from "../../constants";
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
 import { Link } from 'expo-router';
+import { createUser } from '../../lib/appwrite';
 
 const SignUp = () => {
   const [isSubmitting, setSubmitting] = useState(false);
@@ -14,7 +15,9 @@ const SignUp = () => {
     password: "",
   });
 
-  const submit = () => { }
+  const submit = () => {
+    createUser()
+  }
 
   return (
     <SafeAreaView className="bg-primary h-full">
